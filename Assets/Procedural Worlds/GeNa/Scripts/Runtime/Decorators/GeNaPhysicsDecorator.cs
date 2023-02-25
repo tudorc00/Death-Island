45913e45a3d15c49de047a841aeb62dbc849f862
Croitoru Tudor <97614029+ctudor00@users.noreply.github.com>
2023-02-25 23:00:59 +0200

    Ghena

---
+++ Assets/Procedural Worlds/GeNa/Scripts/Runtime/Decorators/GeNaPhysicsDecorator.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+#if UNITY_EDITOR
+using UnityEditor.SceneManagement;
+#endif
+using UnityEngine.SceneManagement;
+
+namespace GeNa.Core
+{
+    /// <summary>
+    /// Decorator for performing Physics-based Spawning
+    /// </summary>
+    public class GeNaPhysicsDecorator : GeNaDecorator
+    {
+        [SerializeField] protected PhysicsSimulatorSettings m_settings = new PhysicsSimulatorSettings();
+        public PhysicsSimulatorSettings Settings
+        {
+            get => m_settings;
+            set => m_settings = value;
+        }
+        public override void OnIngest(Resource resource)
+        {
+            resource.PhysicsEnabled = true;
+        }
+        public override IEnumerator OnSelfSpawned(Resource resource)
+        {
+#if UNITY_EDITOR
+            GeNaSpawnerData spawnerData = resource.SpawnerData;
+            List<ResourceEntity> entities = new List<ResourceEntity>();
+            Vector3 location = transform.position;
+            Vector3 rotation = transform.eulerAngles;
+            Vector3 scale = transform.localScale;
+            ResourceEntity spawnedEntity = GeNaSpawnerInternal.GetResourceEntity(location, rotation, scale, resource);
+            
+            GameObject spawnProgress = GeNaSpawnerInternal.GetSpawnProgressParent(spawnerData);
+            if (spawnedEntity != null)
+            {
+                spawnedEntity.GameObject = gameObject;
+                entities.Add(spawnedEntity);
+                transform.SetParent(spawnProgress.transform);
+            }
+
+            Scene previewScene = EditorSceneManager.NewScene(NewSceneSetup.EmptyScene, NewSceneMode.Additive);
+            m_settings.m_physicsScene = new PhysicsScene();
+
+            var oldScene = spawnProgress.scene;
+
+            SceneManager.MoveGameObjectToScene(spawnProgress, previewScene);
+            
+            // If no longer spawning 
+            if (spawnerData.PhysicsType == Constants.PhysicsType.Resource && !GeNaGlobalReferences.GeNaManagerInstance.Cancel)
+            {
+                IEnumerator simulateMethod = GeNaEvents.Simulate(entities, m_settings, this);
+                while (simulateMethod.MoveNext())
+                {
+                    yield return simulateMethod.Current;
+                }
+            }
+
+            SceneManager.MoveGameObjectToScene(spawnProgress, oldScene);
+            
+            EditorSceneManager.CloseScene(previewScene, true);
+#else
+    yield return null;
+#endif
+        }
+    }
+}
